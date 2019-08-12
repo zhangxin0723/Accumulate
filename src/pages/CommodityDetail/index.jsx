8cc4db43949cf412b1bee0f369234f75b43480a7
cj <1135528371@qq.com>
2019-08-12 21:21:24 +0800

    详情排版

--- src/pages/CommodityDetail/index.jsx
+++ src/pages/CommodityDetail/index.jsx
@@ -1,5 +1,8 @@
 import Taro, { Component } from "@tarojs/taro";
-import { View, Button, Text } from "@tarojs/components";
+import { View, Button, Text, Swiper, SwiperItem } from "@tarojs/components";
+import namedPng from "../../images/1.jpg";
+import namePng1 from "../../images/2.jpg";
+import blackCard from "../../images/黑卡(2).svg";
 // import { observer, inject } from '@tarojs/mobx'
 import "./index.scss";
 
@@ -30,9 +33,42 @@ class CommodityDetail extends Component {
 
   render() {
     return (
-      <View className="detail">
-        <View>
-          <img src="" alt="" />
+      <View className="detailWrap">
+        <Swiper
+          className="test-h"
+          indicatorColor="#999"
+          indicatorActiveColor="#333"
+          circular
+          indicatorDots
+          autoplay
+        >
+          <SwiperItem className="SwiperItem">
+            <View className="demo-text-1 SwiperBanner">
+              <Image src={namedPng} />
+            </View>
+          </SwiperItem>
+          <SwiperItem className="SwiperItem">
+            <View className="demo-text-2 SwiperBanner">
+              <Image src={namePng1} />
+            </View>
+          </SwiperItem>
+          <SwiperItem className="SwiperItem">
+            <View className="demo-text-3 SwiperBanner">
+              <Image src={namedPng} />
+            </View>
+          </SwiperItem>
+        </Swiper>
+        <View className='detailWrapTit'>
+          <View className='detailWrapTitLeft'>
+            <Text className='detailWrapTitMoney'>￥75</Text>
+            <Text>73.02</Text>
+            <Image src={blackCard} />
+          </View>
+          <View>分享</View>
+        </View>
+        <View className="detailLower">
+          <View className="detailLowerLeft">分享赚</View>
+          <View className="detailLowerRight">立即购买</View>
         </View>
       </View>
     );
