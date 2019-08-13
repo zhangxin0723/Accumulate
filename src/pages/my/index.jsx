f8081a380b9c2bb336c4eb4648d985b889ba2946
houfaxing <houfaxing1024@163.com>
2019-08-13 19:05:52 +0800

    '内容初始化'

--- src/pages/my/index.jsx
+++ src/pages/my/index.jsx
@@ -1,159 +1,224 @@
-import Taro, { Component } from '@tarojs/taro'
-import { View, Button, Text } from '@tarojs/components'
-import { observer, inject } from '@tarojs/mobx'
-import others from '../../img/others.svg'
-import payment from '../../img/payment.svg'
-import received from '../../img/received.svg'
-import refund from '../../img/refund.svg'
-import logBg from '../../images/logBg.png'
-import Group from '../../images/member.png'
-import members from '../../images/members.png'
-import yhj from '../../images/yhj.png'
-import kf from '../../images/kf.png'
-import jt from '../../images/jt.png'
-import shouye from '../../images/shouye.png'
-import shop from '../../images/shop.png'
-import active from '../../images/my_active.png'
+import Taro, { Component } from "@tarojs/taro";
+import { View, Button, Text } from "@tarojs/components";
+import { observer, inject } from "@tarojs/mobx";
+import others from "../../img/others.svg";
+import payment from "../../img/payment.svg";
+import received from "../../img/received.svg";
+import refund from "../../img/refund.svg";
+import logBg from "../../images/logBg.png";
+import Group from "../../images/member.png";
+import members from "../../images/members.png";
+import yhj from "../../images/yhj.png";
+import kf from "../../images/kf.png";
+import jt from "../../images/jt.png";
+import shouye from "../../images/shouye.png";
+import shop from "../../images/shop.png";
+import active from "../../images/my_active.png";
 
-import './index.scss'
-
-
-@inject('counterStore')
+import "./index.scss";
+var data = [
+  {
+    name: "待付款",
+    orderStatus: 1,
+    Image: payment
+  },
+  {
+    name: "待发货",
+    orderStatus: 2,
+    Image: others
+  },
+  {
+    name: "待收货",
+    orderStatus: 3,
+    Image: received
+  },
+  {
+    name: "全部",
+    orderStatus: 0,
+  }
+];
+@inject("counterStore")
 @observer
 class My extends Component {
-
   config = {
-    navigationBarTitleText: '首页'
+    navigationBarTitleText: "首页"
+  };
+
+  componentWillMount() {}
+
+  componentWillReact() {
+    console.log("componentWillReact");
   }
 
-  componentWillMount () { }
+  componentDidMount() {}
 
-  componentWillReact () {
-    console.log('componentWillReact')
+  componentWillUnmount() {}
+
+  componentDidShow() {
+    var that = this;
+
+    that.setState({
+      newDtata: data
+    });
   }
 
-  componentDidMount () { }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
+  componentDidHide() {}
 
   increment = () => {
-    const { counterStore } = this.props
-    counterStore.increment()
-  }
+    const { counterStore } = this.props;
+    counterStore.increment();
+  };
 
   decrement = () => {
-    const { counterStore } = this.props
-    counterStore.decrement()
-  }
+    const { counterStore } = this.props;
+    counterStore.decrement();
+  };
 
   incrementAsync = () => {
-    const { counterStore } = this.props
-    counterStore.incrementAsync()
-  }
-
-  render () {
+    const { counterStore } = this.props;
+    counterStore.incrementAsync();
+  };
+  state = {
+    newDtata: []
+  };
+  render() {
     // const { counterStore: { counter } } = this.props
+    const { newDtata } = this.state;
     return (
-      <View className='wrap'>
-      <View className="header">
-         <Image src={logBg}/>
-         <View className="header_name">
-          <ul>
-            <li>
-               <Image src={members}/>
-            </li>
-            <li>
-               <span>
-                 <i>默默许愿</i> 
+      <View className="wrap">
+        <View className="header">
+          <Image src={logBg} />
+          <View className="header_name">
+            <ul>
+              <li>
+                <Image src={members} />
+              </li>
+              <li>
+                <span>
+                  <i>默默许愿</i>
                   <em>
-                    <Image src={Group}/> 
+                    <Image src={Group} />
                   </em>
-               </span>
-               <span>邀请码: C5JQ70<i>复制</i></span>
-            </li>
-          </ul>
-         </View>
-      </View>
-      <View className="center">
-        <View className="center_colse">
+                </span>
+                <span>
+                  邀请码: C5JQ70<i>复制</i>
+                </span>
+              </li>
+            </ul>
+          </View>
+        </View>
+        <View className="center">
+          <View className="center_colse">
             <View className="center_my">
-               <span>我的订单</span>
-               <span>查看全部订单</span>
+              <span>我的订单</span>
+              <span>查看全部订单</span>
             </View>
             <View className="center_payment">
               <ul>
-                <li>
-                  <span><Image src={payment}/></span>
-                  <span>待付款</span>
-                </li>
-                <li>
-                  <span><Image src={others}/></span>
-                  <span>待发货</span>
-                </li>
-                <li>
-                  <span><Image src={received}/></span>
-                  <span>待收货</span>
-                </li>
-                <li>
-                  <span><Image src={refund}/></span>
-                  <span>退货退款</span>
-                </li>
+                {newDtata &&
+                  newDtata.map((item, index) => {
+                    return (
+                      item.Image&&<View
+                        onClick={() => {
+                          Taro.navigateTo({
+                            url: `/pages/payment/index?orderStatus=${
+                              item.orderStatus
+                            }&name=${item.name}`
+                          });
+                        }}
+                      > 
+                        <li key={item.orderStatus}>
+                          <span>
+                            <Image src={item.Image} />
+                          </span>
+                          <span>{item.name}</span>
+                        </li>
+                      </View>
+                    );
+                  })}
               </ul>
             </View>
             <View className="discount">
               <ol>
                 <li>
-                   <Image src={yhj}/>
-                  <span><em>我的优惠券</em><i><Image src={jt}/></i></span>
+                  <Image src={yhj} />
+                  <span>
+                    <em>我的优惠券</em>
+                    <i>
+                      <Image src={jt} />
+                    </i>
+                  </span>
                 </li>
                 <li>
-                <Image src={kf}/>
-                  <span><em>关于我们</em><i><Image src={jt}/></i></span>
-               </li>
-               <li>
-                  <Image src={yhj}/>
-                  <span><em>我的分享</em><i><Image src={jt}/></i></span>
-               </li>
-               <li>
-               <Image src={kf}/>
-                  <span><em>联系客服</em><i><Image src={jt}/></i></span>
-               </li>
-               <li>
-                  <Image src={yhj}/>   
-                  <span><em>设置</em><i><Image src={jt}/></i></span>
-               </li>
+                  <Image src={kf} />
+                  <span>
+                    <em>关于我们</em>
+                    <i>
+                      <Image src={jt} />
+                    </i>
+                  </span>
+                </li>
+                <li>
+                  <Image src={yhj} />
+                  <span>
+                    <em>我的分享</em>
+                    <i>
+                      <Image src={jt} />
+                    </i>
+                  </span>
+                </li>
+                <li>
+                  <Image src={kf} />
+                  <span>
+                    <em>联系客服</em>
+                    <i>
+                      <Image src={jt} />
+                    </i>
+                  </span>
+                </li>
+                <li>
+                  <Image src={yhj} />
+                  <span>
+                    <em>设置</em>
+                    <i>
+                      <Image src={jt} />
+                    </i>
+                  </span>
+                </li>
               </ol>
             </View>
             <p>您的邀请人:Michael先生</p>
+          </View>
         </View>
-      </View>
-      <View className="footer">
-         <ol>
-           <li>
-             <span><Image src={shouye}/></span>
-             <span>首页</span>
-           </li>
-           <li>
-           <span><Image src={shop}/></span>
-             <span>购物车</span>
-           </li>
-           <li>
-             <span><Image src={active}/></span>
-             <span>我的</span>
-           </li>
-         </ol>
-      </View>
+        <View className="footer">
+          <ol>
+            <li>
+              <span>
+                <Image src={shouye} />
+              </span>
+              <span>首页</span>
+            </li>
+            <li>
+              <span>
+                <Image src={shop} />
+              </span>
+              <span>购物车</span>
+            </li>
+            <li>
+              <span>
+                <Image src={active} />
+              </span>
+              <span>我的</span>
+            </li>
+          </ol>
+        </View>
         {/* <Button onClick={this.increment}>+</Button>
         <Button onClick={this.decrement}>-</Button>
         <Button onClick={this.incrementAsync}>Add Async</Button>
         <Text>{counter}</Text> */}
       </View>
-    )
+    );
   }
 }
 
-export default My 
+export default My;
--- src/pages/payment/index.jsx
+++ src/pages/payment/index.jsx
@@ -1,19 +1,42 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Button, Text } from '@tarojs/components'
 import { observer, inject } from '@tarojs/mobx'
-
+import withWeapp from '@tarojs/with-weapp'
+import FooterCont from '../../component/footerCont'
 import './index.scss'
+var data = [
+  {
+    name: "全部",
+    orderStatus: 0,
+  },
+  {
+    name: "待付款",
+    orderStatus: 1,
+  },
+  {
+    name: "待发货",
+    orderStatus: 2,
+  },
+  {
+    name: "待收货",
+    orderStatus: 3,
+  },
 
-
+];
 @inject('counterStore')
 @observer
+
 class Payment extends Component {
 
   config = {
     navigationBarTitleText: '我的订单'
   }
 
-  componentWillMount () { }
+  componentWillMount () { 
+    this.setState({
+      newName:this.$router.params
+    })
+  }
 
   componentWillReact () {
     console.log('componentWillReact')
@@ -21,9 +44,16 @@ class Payment extends Component {
 
   componentDidMount () { }
 
-  componentWillUnmount () { }
+  componentWillUnmount () {
+   }
 
-  componentDidShow () { }
+  componentDidShow () { 
+    var that = this;
+    
+        that.setState({
+          newDtata: data
+        });
+  }
 
   componentDidHide () { }
 
@@ -41,12 +71,42 @@ class Payment extends Component {
     const { counterStore } = this.props
     counterStore.incrementAsync()
   }
-
+  state={
+    newName:"",
+    newDtata:[],
+    newIndex:''
+  }
   render () {
     // const { counterStore: { counter } } = this.props
+    let {newName,newDtata,newIndex}=this.state
     return (
       <View className='wrap'>
-        
+        <View className="made">
+          <ul>
+            
+            {
+              newDtata.map((item,index)=>{
+                return <View style={{
+                   borderBottom:item.orderStatus==newIndex?'3rpx solid #33d6c5':null
+                }} onClick={()=>{
+                    this.setState({
+                      newIndex:item.orderStatus
+                    })
+                }}><li>{item.name}</li></View>
+              })
+            }
+           
+          </ul>
+          <View className="footer">
+             <View className="footer_type">
+                <span>向上加载</span>
+                <View className="footer_cont">
+                   <FooterCont/>
+                </View>
+                <span>向下加载</span>
+             </View>
+          </View>
+        </View>
         {/* <Button onClick={this.increment}>+</Button>
         <Button onClick={this.decrement}>-</Button>
         <Button onClick={this.incrementAsync}>Add Async</Button>
