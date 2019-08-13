5a2b7a065c7342f0bcf24e45655176147fe1298b
cj <1135528371@qq.com>
2019-08-13 21:30:28 +0800

    提交订单排版

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,9 @@
 import Taro, { Component } from "@tarojs/taro";
 import { Provider } from "@tarojs/mobx";
 import Index from "./pages/index";
-
 import counterStore from "./store/counter";
-
 import "./app.scss";
-
-// 如果需要在 h5 环境中开启 React Devtools
-// 取消以下注释：
-// if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
-//   require('nerv-devtools')
-// }
+import "taro-ui/dist/style/index.scss";
 
 const store = {
   counterStore
@@ -19,6 +12,8 @@ const store = {
 class App extends Component {
   config = {
     pages: [
+      //提交订单
+      "pages/PlaceOrder/index",
       //商品详情
       "pages/CommodityDetail/index",
       "pages/index/index"
--- src/pages/CommodityDetail/index.jsx
+++ src/pages/CommodityDetail/index.jsx
@@ -1,10 +1,10 @@
 import Taro, { Component } from "@tarojs/taro";
-import { View, Button, Text, Swiper, SwiperItem } from "@tarojs/components";
+import { View, Text, Swiper, SwiperItem } from "@tarojs/components";
 import namedPng from "../../images/1.jpg";
 import namePng1 from "../../images/2.jpg";
 import blackCard from "../../images/黑卡(2).svg";
-// import { observer, inject } from '@tarojs/mobx'
 import "./index.scss";
+import { AtFloatLayout } from "taro-ui";
 
 // @inject('counterStore')
 // @observer
@@ -12,7 +12,18 @@ class CommodityDetail extends Component {
   config = {
     navigationBarTitleText: "首页详情"
   };
-
+  constructor() {
+    this.state = {
+      isOpened: false
+    };
+  }
+  handleClickBtn() {
+    this.setState(() => {
+      return {
+        isOpened: !this.state.isOpened
+      };
+    });
+  }
   componentWillMount() {}
 
   componentWillReact() {}
@@ -32,6 +43,7 @@ class CommodityDetail extends Component {
   incrementAsync = () => {};
 
   render() {
+    let { isOpened } = this.state;
     return (
       <View className="detailWrap">
         <Swiper
@@ -66,16 +78,82 @@ class CommodityDetail extends Component {
             <Text>73.02</Text>
             <Image src={blackCard} />
           </View>
-          <View className='detailWrapTitShare'>分享赚1.49</View>
+          <View className="detailWrapTitShare">分享赚1.49</View>
         </View>
-        <View className='detailWrapName'>
-          <View>澳洲直邮Swisse钙+维生素D片150粒*2罐</View>
-          <View>快递包邮</View>
+        <View className="detailWrapName">
+          <View className="detailWrapTop">
+            澳洲直邮Swisse钙+维生素D片150粒*2罐洲直邮Swisse钙+维生素D片150粒*2罐
+          </View>
+          <View className="detailWrapBottom">快递包邮</View>
         </View>
+        <View className="detailWrapChoice">
+          <View>
+            选择 <View>默认</View>
+          </View>
+          <Text className="detailWrapChoiceGt">&gt;</Text>
+        </View>
+        <View className="detailWrapChoice">
+          <View>
+            说明 <View className="detailWrapChoicePink">适合幼儿园玩耍</View>
+          </View>
+        </View>
+        <View className="detailWrapChoice">
+          <View>
+            提示
+            <View className="detailWrapChoicePink">
+              西藏自治区，新疆维吾尔自治区不包邮，运费加20.00元
+            </View>
+          </View>
+        </View>
+        <View className="detailWrapList">内容</View>
         <View className="detailLower">
           <View className="detailLowerLeft">分享赚</View>
-          <View className="detailLowerRight">立即购买</View>
+          <View
+            onClick={() => this.handleClickBtn()}
+            className="detailLowerRight"
+          >
+            立即购买
+          </View>
         </View>
+        <AtFloatLayout isOpened={this.state.isOpened} title="颜色、尺码">
+          <View className="detailWrapMask">
+            <View className="detailWrapMaskBanner">
+              <Image src={namePng1} />
+            </View>
+            <View className="detailWrapMaskPrice">
+              <Text className="detailWrapMaskPriceMoney">￥399.50</Text>
+              <Text className="detailWrapMaskPriceStock">库存: 4999</Text>
+            </View>
+          </View>
+          <View className="detailWrapMaskColor">
+            <View className="detailWrapMaskColors">颜色</View>
+            <View className="detailWrapMaskColorSize">
+              <Text className="active">灰色</Text>
+              <Text>灰色灰色</Text> <Text>灰色</Text>
+              <Text>灰色</Text>
+              <Text>灰色</Text>
+              <Text>灰色</Text>
+            </View>
+          </View>
+          <View className="detailWrapMaskColor">
+            <View className="detailWrapMaskColors">尺码</View>
+            <View className="detailWrapMaskColorSize">
+              <Text className="active">L(170/92A)</Text>
+              <Text>L(170/92A)</Text>
+              <Text>L(170/92A)</Text>
+              <Text>L(170/92A)</Text>
+            </View>
+          </View>
+          <View className="detailWrapNum">
+            <View className="detailWrapNumLeft">数量</View>
+            <View className="detailWrapNumRight">
+              <Text className="detailWrapNumRightReduce">–</Text>
+              <Text>1</Text>
+              <Text className="detailWrapNumRightAdd">+</Text>
+            </View>
+          </View>
+          <View className="detailWrapNumBtn">确定</View>
+        </AtFloatLayout>
       </View>
     );
   }
